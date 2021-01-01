c7ec1cf48b14b54724665308fefcbf88df8a895d
Anish Aggarwal <anish.5@hotmail.com>
2020-12-31 23:08:40 -0500

    Update fiddling.py
    Understood a bit more about DNN. Now starting the identification of the neutrons the Snake AI Neural Net is going to use.

--- Classic Snake AI/Deep Neural Network/fiddling.py
+++ Classic Snake AI/Deep Neural Network/fiddling.py
@@ -3,16 +3,28 @@ import numpy as np
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
+def sigDeriv(x):
+    return x + (1 - x)
 training = np.array([[0,0,1],
                      [1,1,1],
-                     [0,1,1]]).T
+                     [0,1,1]])
+tout = np.array([[0,1,0]]).T
 np.random.seed(1)
 
-synaptic_weights = 2*random.random((3,1)) -1
+synaptic_weights = 2*np.random.random((3,1)) -1
 print("Random start weights")
 print(synaptic_weights)
-for i in range(1):
-    i_layer = training_inputs
+
+for i in range(100000):
+    i_layer = training
     outputs = sigmoid(np.dot(i_layer,synaptic_weights))
+
+    #Adjustments of Weights of Edges
+    error = tout - outputs
+    adjust = error * sigDeriv(outputs)
+    synaptic_weights += np.dot(i_layer.T,adjust)
+
+print ("Weights After")
+print(synaptic_weights)
 print("Out aft Training")
 print(outputs)
