cec13fd855752eefb620cf4651f08eb2f843aa76
Anish Aggarwal <anish.5@hotmail.com>
2021-01-07 23:49:13 -0500

    Update game-with-ai.py
    Changed to work with Neural Net. However, debugging required as snake does not make "proper" moves.

--- Classic Snake AI/SL Deep Neural Network/game-with-ai.py
+++ Classic Snake AI/SL Deep Neural Network/game-with-ai.py
@@ -2,16 +2,14 @@ import pygame
 from random import *
 import time
 import itertools
+import sys
+import math
+from ANN import NeuralNet
+import os
+import csv
 
 pygame.init()
-
-gridX = 64
-gridY = 36
-gridBSize=20
-screen = pygame.display.set_mode([gridX*gridBSize,gridY*gridBSize])
-
-
-
+sys.setrecursionlimit(3000)
 
 class Snake:
     heading = 0 # 0: Right, 1: Up, 2: Left, 3: Down
@@ -20,30 +18,100 @@ class Snake:
     speed = 1
     partSize = 0
     foodPosition = []
-    def __init__(self,speed,gridSizeX, gridSizeY,gridBSize):
-        self.snakeParts = [[3,3]]
+    def __init__(self,speed,allP,gridBSize):
+        self.snakeParts = [choice(allP)]
         self.heading = 0
         self.partSize = gridBSize
-        self.foodPosition = self.getFoodPos(gridSizeX,gridSizeY,self.snakeParts)
+        self.foodPosition = self.getFoodPos(allPos,self.snakeParts)
     
-    def move_forw(self,gX,gY):
+    def hit_wall(self,direc,head,gX,gY):
+        maxD = math.sqrt((direc[0]*gX)**2+(direc[1]*gY)**2)
+
+        start = head[:]
+        dist = 0
+        while True:
+            start[0]+=direc[0]
+            start[1]+=direc[1]
+            if 0<=start[0]<gX and 0<=start[1]<gY:
+                dist+=1
+            else:
+                break
+        return dist/maxD
+
+    def hit_fruit(self,direc,head,gX,gY,foodP):
+        maxD = math.sqrt((direc[0]*gX)**2+(direc[1]*gY)**2)
+
+        start = head[:]
+        dist = 0
+        while True:
+            start[0]+=direc[0]
+            start[1]+=direc[1]
+            if 0<=start[0]<gX and 0<=start[1]<gY and start!=foodP:
+                dist+=1
+            elif start!=foodP:
+                dist = maxD*100
+                break
+            else:
+                break
+        return dist/maxD
+
+    def hit_self(self,direc,head,gX,gY):
+        maxD = math.sqrt((direc[0]*gX)**2+(direc[1]*gY)**2)
+
+        start = head[:]
+        dist = 0
+        while True:
+            start[0]+=direc[0]
+            start[1]+=direc[1]
+            if 0<=start[0]<gX and 0<=start[1]<gY and start not in self.snakeParts:
+                dist+=1
+            elif start not in self.snakeParts:
+                dist = maxD*100
+                break
+            else:
+                break
+        return dist/maxD
+    
+    def get_data(self,head,gX,gY,foodP):
+        direcs = [[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1],[0,1],[1,1]]
+
+        new_obs = [self.hit_wall(direcs[0],head,gX,gY),self.hit_fruit(direcs[0],head,gX,gY,foodP),self.hit_self(direcs[0],head,gX,gY),
+                   self.hit_wall(direcs[1],head,gX,gY),self.hit_fruit(direcs[1],head,gX,gY,foodP),self.hit_self(direcs[1],head,gX,gY),
+                   self.hit_wall(direcs[2],head,gX,gY),self.hit_fruit(direcs[2],head,gX,gY,foodP),self.hit_self(direcs[2],head,gX,gY),
+                   self.hit_wall(direcs[3],head,gX,gY),self.hit_fruit(direcs[3],head,gX,gY,foodP),self.hit_self(direcs[3],head,gX,gY),
+                   self.hit_wall(direcs[4],head,gX,gY),self.hit_fruit(direcs[4],head,gX,gY,foodP),self.hit_self(direcs[4],head,gX,gY),
+                   self.hit_wall(direcs[5],head,gX,gY),self.hit_fruit(direcs[5],head,gX,gY,foodP),self.hit_self(direcs[5],head,gX,gY),
+                   self.hit_wall(direcs[6],head,gX,gY),self.hit_fruit(direcs[6],head,gX,gY,foodP),self.hit_self(direcs[6],head,gX,gY),
+                   self.hit_wall(direcs[7],head,gX,gY),self.hit_fruit(direcs[7],head,gX,gY,foodP),self.hit_self(direcs[7],head,gX,gY)]
+
+        return new_obs
+
+            
+    def move_forw(self,allPos,gridX,gridY,ann):
         head = self.snakeParts[0][:]
-        head[0]+=self.speed*self.dirs[self.heading][0]
-        head[1]+=self.speed*self.dirs[self.heading][1]
-        if head == self.foodPosition:
-            self.foodPosition = self.getFoodPos(gX,gY,self.snakeParts)
+        obs = self.get_data(head,gridX,gridY,self.foodPosition)
+        direc = ann.predict([obs])
+        head = [head[0]+direc[0],head[1]+direc[1]]
+        self.heading = self.dirs.index(direc)
+        
+        if self.foodPosition in self.snakeParts:
+            self.foodPosition = self.getFoodPos(allPos,self.snakeParts)         
         else:
             self.snakeParts.pop()
         self.snakeParts=[head]+self.snakeParts
 
-    def draw_snake_food(self,display):
+    def draw_snake_food(self,display,done):
         display.fill((0,0,0))
+        sCol = (0,255,0)
+        fCol = (255,255,0)
+        if done:
+            fCol = (0,0,255)
         for part in self.snakeParts:
-            pygame.draw.rect(display,(0,255,0), (part[0]*self.partSize,part[1]*self.partSize,
+            pygame.draw.rect(display,sCol, (part[0]*self.partSize,part[1]*self.partSize,
                                                      self.partSize,self.partSize))
-        pygame.draw.rect(display,(255,255,0), (self.foodPosition[0]*self.partSize,self.foodPosition[1]*self.partSize,
-                                              self.partSize,self.partSize))
-                                              
+
+        pygame.draw.rect(display,fCol, (self.foodPosition[0]*self.partSize,self.foodPosition[1]*self.partSize,
+                                          self.partSize,self.partSize))                                              
             
             
     def change_heading(self,key):
@@ -56,10 +124,10 @@ class Snake:
         elif key==pygame.K_DOWN:
             self.heading = 3
 
-    def getFoodPos(self,gX,gY,posTaken):
-        allPositions = list(itertools.product(*[[x for x in range(gX)],[y for y in range(gY)]]))
-        shuffle(allPositions)
-        for pos in allPositions:
+    def getFoodPos(self,allPositions,posTaken):
+        allPo = allPositions[:]
+        shuffle(allPo)
+        for pos in allPo:
             if pos not in posTaken:
                 return [pos[0],pos[1]]
         return [0,0] #no positions left. perfect snake.
@@ -77,20 +145,34 @@ class Snake:
         elif not (0<=head[0]<gX and 0<=head[1]<gY):
             return True
         else:
-            return False
-    
-mySnake = Snake(1,gridX,gridY,gridBSize)
+                return False
+
+gridX = 20
+gridY = 20
+gridBSize= 20
+screen = pygame.display.set_mode([gridX*gridBSize,gridY*gridBSize])
+  
+print(os.listdir("."))
+ann = NeuralNet()
+
+allPos = []
+for y in range(gridY):
+    for x in range(gridX):
+        allPos.append([x,y])
+
+
+mySnake = Snake(1,allPos,gridBSize)
 running = True
+gameDone = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYUP:
-            mySnake.change_heading(event.key)
     if mySnake.check_death(gridX,gridY):
-        running=False
-    mySnake.move_forw(gridX,gridY)
-    mySnake.draw_snake_food(screen)
-    time.sleep(0.04)
+        gameDone = True
+    if not gameDone:
+        mySnake.move_forw(allPos,gridX,gridY,ann)
+    mySnake.draw_snake_food(screen,gameDone)
+    time.sleep(0.05)
     pygame.display.flip()
 pygame.quit()
